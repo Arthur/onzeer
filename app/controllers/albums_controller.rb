7306085a96af1de93cc93cc8d3157fe2d9b02e34
Arthur PETRY <arthur@imac-de-arthur-petry.local>
2009-09-26 16:13:33 +0200

    Albums pagination

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,7 +1,7 @@
 class AlbumsController < ApplicationController
 
   def index
-    @albums = Album.all
+    @albums = Album.paginate(params.merge(:order => 'artist, name', :per_page => 10))
   end
 
   def show
