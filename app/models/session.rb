2502f42d428b90d689a67a27e15b27b4ceab5a6b
Arthur Petry <arthur.petry@bolloretelecom.eu>
2009-10-10 15:17:06 +0200

    Login with normal OpenID too

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,10 +1,20 @@
 class Session
 
   attr_accessor :email
+  attr_accessor :openid_url
+
+  def gmail?
+    openid_url.blank?
+  end
+
+  def openid_url_or_gmail
+    openid_url.blank? ? "https://www.google.com/accounts/o8/id" : openid_url
+  end
 
   def initialize(attributes={})
     attributes ||= {}
     self.email = attributes[:email]
+    self.openid_url = attributes[:openid_url]
   end
 
 end
