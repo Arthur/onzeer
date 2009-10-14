1c50ad8f31c6e116c6761b4400a407cff1ddce7d
Arthur PETRY <arthur@imac-de-arthur-petry.local>
2009-10-14 22:19:01 +0200

    last_listenings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   skip_before_filter :ensure_activated
 
   def index
+    @last_listenings = TrackListening.find(:all, :order => 'created_at DESC', :limit => 10)
     @last_comments = UserComment.find(:all, :order => 'created_at DESC', :limit => 10)
     @last_albums = Album.find(:all, :order => 'created_at DESC', :limit => 10)
   end
