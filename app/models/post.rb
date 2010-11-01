426eaadd320519ae6bcd16d474d98dd3e138c88b
Arthur PETRY <arthur.petry@bolloretelecom.eu>
2010-11-01 16:32:13 +0100

    blog post : fix date in comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,14 @@ class Post
 
   many :comments
 
+  before_save :timestamps_in_comments
+
+  def timestamps_in_comments
+    current_time = Time.now.utc
+    comments.each do |comment|
+      comment.created_at ||= current_time
+      comment.updated_at ||= current_time
+    end
+  end
+
 end
