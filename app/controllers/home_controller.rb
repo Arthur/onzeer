12076fa5ae7c97e851456e8461d09135a4cddc53
Arthur PETRY <arthur.petry@bolloretelecom.eu>
2010-11-01 16:31:49 +0100

    home : link to blog

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   skip_before_filter :ensure_activated
 
   def index
+    @last_blog_post = Post.last
     @last_listenings = TrackListening.find(:all, :order => 'created_at DESC', :limit => 10)
     @last_comments = UserComment.find(:all, :order => 'created_at DESC', :limit => 10)
     @last_albums = Album.find_last
