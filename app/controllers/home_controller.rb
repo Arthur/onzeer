f5cbe4ec9188664f52710d5975b5c55eefd14089
Arthur PETRY <arthur.petry@bolloretelecom.eu>
2010-12-11 18:41:30 +0100

    Users by upload nbs !

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
     @last_listenings = TrackListening.find(:all, :order => 'created_at DESC', :limit => 10)
     @last_comments = UserComment.find(:all, :order => 'created_at DESC', :limit => 10)
     @last_albums = Album.find_last
+    @users = User.all.sort_by{|u| u.last_uploaded_albums.empty? ? 0 : u.last_uploaded_albums.total_entries}.reverse
   end
 
 end
