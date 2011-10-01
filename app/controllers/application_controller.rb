7de2bc62f20f1bf6f0aff9a0dd64d51c6d674b14
Arthur PETRY <arthur.petry@gmail.com>
2011-10-01 11:57:20 +0200

    no protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  
+
+  # no protect_from_forgery for now, not really needed, or have to had csrf_meta_tag, and use it in js
+  # protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
